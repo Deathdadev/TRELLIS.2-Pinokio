3a84eb6fe9d673c4c5099f434aea98f4ac88951b
Thomas Eric <thombelcar@gmail.com>
2026-01-22 08:30:57 -0300

    Add the pygltflib package for compat with update

--- install.js
+++ install.js
@@ -47,7 +47,7 @@ module.exports = {
                 venv: "venv",
                 path: "app",
                 message: [
-                    "uv pip install wheel setuptools imageio imageio-ffmpeg tqdm easydict opencv-python-headless ninja trimesh transformers gradio==5.50.0 tensorboard pandas lpips zstandard kornia timm plyfile numpy spconv-cu124"
+                    "uv pip install wheel setuptools imageio imageio-ffmpeg tqdm easydict opencv-python-headless ninja trimesh transformers gradio==5.50.0 tensorboard pandas lpips zstandard kornia timm plyfile numpy spconv-cu124 pygltflib"
                 ]
             }
         },
