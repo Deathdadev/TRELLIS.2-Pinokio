a31ffdb686ce55d2b11cde15f7f516f9835977fd
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2026-07-21 05:42:23 +0200

    pin opencv-python-headelss
    opencv-python-headless 5.x breaks trellis 2 and needs to be pinned to a version prior to opencv-python-headless 5

--- install.js
+++ install.js
@@ -47,7 +47,7 @@ module.exports = {
                 venv: "venv",
                 path: "app",
                 message: [
-                    "uv pip install wheel setuptools imageio imageio-ffmpeg tqdm easydict opencv-python-headless ninja trimesh transformers==4.57.3 gradio==5.50.0 tensorboard pandas lpips zstandard kornia timm plyfile numpy pygltflib pymeshlab"
+                    "uv pip install wheel setuptools imageio imageio-ffmpeg tqdm easydict opencv-python-headless==4.13.0.90 ninja trimesh transformers==4.57.3 gradio==5.50.0 tensorboard pandas lpips zstandard kornia timm plyfile numpy pygltflib pymeshlab"
                 ]
             }
         },
